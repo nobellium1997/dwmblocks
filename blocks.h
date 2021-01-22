a6f07fcc4de83228ff9ac052f53b2568e4869953
Nobel <nobarakat@gmail.com>
2021-01-22 08:43:49 -0700

    mutmic now in scripts dir

--- blocks.h
+++ blocks.h
@@ -3,6 +3,8 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	// {"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
+	{"", "~/Scripts/micmute.sh",					1,		0},
+
 	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
 
 	{"", "acpi | cut -d' ' -f3-",					60,		0},
@@ -10,8 +12,6 @@ static const Block blocks[] = {
 	{"", "nmcli -t -f name connection show --active",					60,		0},
 
 	{"Load Avg: ", "cat /proc/loadavg | awk -F ' ' '{print $1}'",					60,		0},
-
-	{"", "~/micmute.sh",					1,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
